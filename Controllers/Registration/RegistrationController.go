e7f5b4f5be61f19e86f19eeecc72639fe85e5044
Sergei Mikhailovskii <uSQcBQpfxPqTLDiwutWvSX>
2021-12-06 23:43:24 +0300

    Added logic to get registration endpoint

--- Controllers/Registration/RegistrationController.go
+++ Controllers/Registration/RegistrationController.go
@@ -1,13 +1,27 @@
 package Registration
 
 import (
+	"TestProject/Models/Base"
+	"TestProject/Models/Onboarding"
+	"TestProject/Util"
 	"github.com/gin-gonic/gin"
 )
 
 func GetStep(c *gin.Context) {
-	//var responseStatusCode = -1
-	//var response Base.Response
-	//response.Flow = Util.Onboarding
+	var responseStatusCode = -1
+	var response Base.Response
+	response.Flow = Util.Registration
+	step, err := getRegistrationStep(c)
+	if err != nil {
+		response.Errors = []Base.Error{
+			{
+				Field:   "registration",
+				Message: err.Error(),
+			},
+		}
+	}
+	response.Data = Onboarding.Response{Step: step}
+	c.JSON(responseStatusCode, response)
 }
 
 func SubmitStep(c *gin.Context) {
--- Controllers/Registration/RegistrationHandler.go
+++ Controllers/Registration/RegistrationHandler.go
@@ -4,15 +4,29 @@ import (
 	"TestProject/Models"
 	"TestProject/Models/Base"
 	"TestProject/Models/Registration"
+	"TestProject/Util"
 	"github.com/gin-gonic/gin"
 )
 
 func IsRegistrationFinished(c *gin.Context) bool {
 	primaryRegistrationRecord := getPrimaryRegistrationRecord(c)
+	return areFirstStepFieldsFilled(primaryRegistrationRecord) &&
+		primaryRegistrationRecord.Sex != ""
+}
+
+func getRegistrationStep(c *gin.Context) (string, error) {
+	primaryRegistrationRecord := getPrimaryRegistrationRecord(c)
+	if !areFirstStepFieldsFilled(primaryRegistrationRecord) {
+		return "STEP_CREATE_ACCOUNT", nil
+	} else {
+		return "", Util.RegistrationStepNotFound
+	}
+}
+
+func areFirstStepFieldsFilled(primaryRegistrationRecord Registration.PrimaryInfo) bool {
 	return primaryRegistrationRecord.FirstName != "" &&
 		primaryRegistrationRecord.LastName != "" &&
 		primaryRegistrationRecord.Email != "" &&
-		primaryRegistrationRecord.Sex != "" &&
 		primaryRegistrationRecord.Password != ""
 }
 
